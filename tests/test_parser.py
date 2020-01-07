b5122067ba83ff2a87190c46993392e9ff3b6085
Bogdan <bogdan.danilyuk@eosda.com>
2020-01-07 22:29:31 +0200

    provide parser

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,29 +1,30 @@
 import unittest
 from utils import parse_input, parse_input_file
-from tests import testdata
+from tests import parser_testdata
 
 
 
 class TestInputStringParser(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.test_data = [testdata.TEST_DATA_STRING_1, testdata.TEST_DATA_STRING_2]
+        self.test_data = [parser_testdata.TEST_DATA_STRING_1, parser_testdata.TEST_DATA_STRING_2]
+        self.bad_data = parser_testdata.TEST_DATA_STRING_BAD
 
     def test_parse_input(self):
         for idata in self.test_data:
             with self.subTest(idata=idata):
                 parsed_data = parse_input(idata.input)
-                self.assertEqual(parsed_data, idata.output)
+                self.assertEqual(idata.output, parsed_data)
 
 
 class TestInputFileParser(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.test_data = [testdata.TEST_DATA_FILE_1, testdata.TEST_DATA_FILE_2]
+        self.test_data = [parser_testdata.TEST_DATA_FILE_1, parser_testdata.TEST_DATA_FILE_2]
 
     def test_parse_input(self):
         for idata in self.test_data:
             with self.subTest(idata=idata):
                 with open(idata.input) as f:
                     parsed_data = parse_input_file(f)
-                    self.assertEqual(parsed_data, idata.output)
+                    self.assertEqual(idata.output, parsed_data)
--- utils.py
+++ utils.py
@@ -1,8 +1,18 @@
+from exceptions import ParseError
+from typing import List, IO
+
+def parse_input(input_string: str, delimiter: str = ' ') -> List:
+    try:
+        splitted_lines = filter(lambda x: x != '', input_string.splitlines())
+        output_arr = []
+        for line in splitted_lines:
+            new_line = [(int(val[0]), int(val[1])) for val in line.split(delimiter)]
+            output_arr.append(new_line)
+        return output_arr
+    except Exception as e:
+        raise ParseError(f"Can't parse data: {str(e)}")
 
 
-def parse_input(input_string):
-    pass
-
-
-def parse_input_file(input_fp):
-    pass
+def parse_input_file(f: IO, delimiter: str = ' ') -> List:
+    text = f.read()
+    return parse_input(text, delimiter)
