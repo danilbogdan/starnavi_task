6f2ff0e388ea544ed16eb0699818dfb7e6ec20f3
Bogdan <bogdan.danilyuk@eosda.com>
2020-01-08 20:29:31 +0200

    formatting

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,6 +1,7 @@
 from treasure_hunt_functional import get_item, find_treasure
 import unittest
 
+
 class TestFunctionalTreasureHunt(unittest.TestCase):
 
     def test_get_item(self):
@@ -21,7 +22,7 @@ class TestFunctionalTreasureHunt(unittest.TestCase):
             [(4, 2), (2, 2), (4, 3), (3, 2), (4, 1)],
             [(5, 1), (2, 3), (3, 3), (5, 4), (1, 5)],
         ]
-        expected_result = [(1,1), (5,5), (1,5), (2,1), (4,4), (3,2), (1,3), (2,5), (4,3)]
+        expected_result = [(1, 1), (5, 5), (1, 5), (2, 1), (4, 4), (3, 2), (1, 3), (2, 5), (4, 3)]
         self.assertEqual(expected_result, find_treasure(test_arr))
 
     def test_find_treasure_fails(self):
